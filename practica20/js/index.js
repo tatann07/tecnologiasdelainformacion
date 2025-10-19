42b5625487eb83cb091c6f3b2599179b4bc2a36f
tatann07 <babytxtan@gmail.com>
2025-10-18 22:56:55 -0600

    Subi la practica 20
    Subi la practica 20

---
+++ practica20/js/index.js
@@ -0,0 +1,15 @@
+// Suma
+let suma = 200 + 300;
+console.log("El resultado de la suma de 200 + 300 es: " + suma);
+
+// Resta
+let resta = 200 - 300;
+console.log("El resultado de la resta de 200 - 300 es: " + resta);
+
+// Multiplicación
+let multiplicacion = 200 * 300;
+console.log("El resultado de la multiplicación de 200 por 300 es: " + multiplicacion);
+
+// División
+let division = 200 / 300;
+console.log("El resultado de la división de 200 entre 300 es: " + division);
